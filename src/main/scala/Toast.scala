029971c0a769bc9c9137cfb11593019a392e973b
pokutuna <popopopopokutuna@gmail.com>
2012-12-03 18:56:51 +0900

    Toastを中央に

--- src/main/scala/Toast.scala
+++ src/main/scala/Toast.scala
@@ -1,7 +1,12 @@
 package com.pokutuna.n7logger
 
 import _root_.android.widget.Toast
+import _root_.android.view.Gravity
 
 object ToastUtil {
-  def say(message: String) = Toast.makeText(App.getContext, message, Toast.LENGTH_LONG).show()
+  def say(message: String) = {
+    val toast = Toast.makeText(App.getContext, message, Toast.LENGTH_LONG)
+    toast.setGravity(Gravity.AXIS_CLIP, 0, 0)
+    toast.show()
+  }
 }
